b7cb6f03291c48232960b8e96bc66d27b6223acc
Timo Hocker <timo@scode.ovh>
2024-03-23 13:44:28 +0000

    simultaneous starting

--- lib/commands.ts
+++ lib/commands.ts
@@ -9,62 +9,100 @@ import { Store } from './store';
 import { exec_command } from './exec';
 import { Service } from './classes/Service';
 import { pull_image } from './docker_interface';
+import { delay } from './util';
 
-async function init_structure (store: Store): Promise<void> {
+async function init_structure(store: Store): Promise<void> {
   for (const volume of store.volumes)
-    await volume.create ();
+    await volume.create();
   for (const network of store.networks)
-    await network.create ();
+    await network.create();
 }
 
-export async function do_up (
+export async function do_up(
   store: Store,
   include_passive: boolean,
   pull: boolean
 ): Promise<void> {
-  await init_structure (store);
+  await init_structure(store);
   if (pull)
-    await do_pull (store);
-  for (const service of store.services) {
-    if (service.passive && !include_passive)
+    await do_pull(store);
+
+  const services = [...store.services];
+  const queue: Service[] = [];
+  const started: string[] = [];
+  let index = 0;
+  while (services.length > 0) {
+    index %= services.length;
+    const service = services[index];
+    if (service.passive && !include_passive) {
+      services.splice(index, 1);
+    }
+
+    const waiting_for = service.depends_on.filter(dep => {
+      const active = queue.filter(started => {
+        started.name === dep
+      }).length;
+      return active === 0;
+    });
+    if (waiting_for.length > 0)
       continue;
-    await service.up ();
+
+    queue.push(...services.splice(index, 1));
   }
+
+  const threads = [];
+  for (let i = 0; i < 4; i++) {
+    threads.push(new Promise(async (res) => {
+      while (queue.length > 0) {
+        const service = queue.shift();
+        let waiting_for = 0;
+        do {
+          waiting_for = service?.depends_on.filter(dep => {
+            started.indexOf(dep) < 0;
+          }).length;
+          await delay(100);
+        } while (waiting_for > 0);
+        await service?.up();
+      }
+      res();
+    }));
+  }
+  await Promise.all(threads);
 }
 
-export async function do_down (store: Store): Promise<void> {
-  for (const service of [ ...store.services ].reverse ())
-    await service.down ();
+export async function do_down(store: Store): Promise<void> {
+  for (const service of [...store.services].reverse())
+    await service.down();
 }
 
-export async function do_pull (store: Store): Promise<void> {
+export async function do_pull(store: Store): Promise<void> {
   const all_images: string[] = [];
   const buildable: Service[] = [];
   for (const service of store.services) {
-    all_images.push (...service.images);
+    all_images.push(...service.images);
     if (service.buildable)
-      buildable.push (service);
+      buildable.push(service);
   }
 
-  const images = all_images.filter ((v, i, a) => a.indexOf (v) === i);
+  const images = all_images.filter((v, i, a) => a.indexOf(v) === i);
 
   const tasks = [];
   const task_list = new TaskListVertical;
 
   for (const buildable_service of buildable) {
     const tl = new TaskListHorizontal;
-    task_list.tasks.push (tl);
+    task_list.tasks.push(tl);
     tl.label = `Building ${buildable_service.name}`;
     tl.label_length = 20;
     tl.display_percentage = false;
     const task = new Task;
-    tl.tasks.push (task);
+    tl.tasks.push(task);
     task.progress = 0.5;
-    tasks.push (exec_command ('docker', [
+    tasks.push(exec_command('docker', [
       'compose',
       'build'
     ], buildable_service.directory)
-      .then (() => {
+      .then(() => {
         task.progress = 1;
         task.completed = true;
       }));
@@ -72,41 +110,41 @@ export async function do_pull (store: Store): Promise<void> {
 
   for (const pullable of images) {
     const tl = new TaskListHorizontal;
-    task_list.tasks.push (tl);
+    task_list.tasks.push(tl);
     tl.label_length = 20;
-    tasks.push (pull_image (pullable, tl));
+    tasks.push(pull_image(pullable, tl));
   }
 
-  task_list.update ();
+  task_list.update();
 
-  await Promise.all (tasks);
+  await Promise.all(tasks);
 }
 
-export async function do_create_filter (store: Store): Promise<void> {
+export async function do_create_filter(store: Store): Promise<void> {
   const filter_lines = [];
-  const backup_volumes = store.volumes.filter ((volume) => volume.backup)
-    .sort ((a, b) => a.name.localeCompare (b.name));
+  const backup_volumes = store.volumes.filter((volume) => volume.backup)
+    .sort((a, b) => a.name.localeCompare(b.name));
   for (const volume of backup_volumes) {
     if (volume.backup_include.length > 0) {
       for (const include of volume.backup_include) {
-        const link = path.normalize (`/${volume.name}/_data/${include}`)
-          .replace (/\\/gu, '/');
-        filter_lines.push (`+ ${link}`);
+        const link = path.normalize(`/${volume.name}/_data/${include}`)
+          .replace(/\\/gu, '/');
+        filter_lines.push(`+ ${link}`);
       }
     }
     else {
-      filter_lines.push (`+ /${volume.name}/`);
+      filter_lines.push(`+ /${volume.name}/`);
     }
   }
-  filter_lines.push ('- /*');
+  filter_lines.push('- /*');
 
   for (const volume of backup_volumes) {
     for (const exclude of volume.backup_exclude) {
-      const link = path.normalize (`/${volume.name}/_data/${exclude}`)
-        .replace (/\\/gu, '/');
-      filter_lines.push (`- ${link}`);
+      const link = path.normalize(`/${volume.name}/_data/${exclude}`)
+        .replace(/\\/gu, '/');
+      filter_lines.push(`- ${link}`);
     }
   }
 
-  await fs.writeFile ('filter', filter_lines.join ('\n'));
+  await fs.writeFile('filter', filter_lines.join('\n'));
 }
